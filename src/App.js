d9587cfd1168c7a28247237b49d43d95a3437407
Yan <alexqin14@gmail.com>
2021-02-09 19:47:38 -0600

    Style some componest
    React Functional componet

--- src/App.js
+++ src/App.js
@@ -4,9 +4,24 @@ import {Greeting} from './Greeting';
 // JS modules
 
 const App = () => {
+
+  const styles = {
+    container: {
+      padding: 20
+      , backgroundColor: new Date().getSeconds() % 2 == 0 ? "lightblue" : "pink"
+    }
+    // DOM css property name tenieray part three parts explicit comparison, if it's true/false 
+    // "?" vs ":" pendatic not a statment, it's a expression
+    , heading: {
+      textTransform: "uppercase"
+    }
+  };
   return (
-    <>
-    <h1>
+    <div
+      style={styles.container}
+    
+    >
+    <h1 style={{padding:40}}>
       Hello React!!!
     </h1>
 
@@ -14,7 +29,7 @@ const App = () => {
     <Greeting name="Mike" favoriteNumber={42}/>
     <Greeting name="Rod" favoriteNumber={43}/>
     {/* <Greeting name="Brandon" favoriteNumber={3}/> */}
-  </>
+  </div>
   );
 }
 
