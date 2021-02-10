7c6642d4bf442a56313220c840c72fd04b6ca8be
Yan <alexqin14@gmail.com>
2021-02-09 18:21:33 -0600

    learn what React fragment
    <>/</>

--- src/App.js
+++ src/App.js
@@ -3,10 +3,18 @@ import './App.css';
 
 const App = () => {
   return (
+    <>
     <h1>
-      Hello React!
+      Hello React!!!
     </h1>
-  
+    <h2>
+      Hello Mike!!!
+    </h2>
+    <h2>
+      Hello Ray!!!
+    </h2>
+    
+  </>
   );
 }
 
