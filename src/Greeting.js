bc654ccacf773da6a30f96057f19709c837f6cf4
Yan <alexqin14@gmail.com>
2021-02-09 18:53:45 -0600

    Passed an array prop
    Also showed boolean short circuit operator

--- src/Greeting.js
+++ src/Greeting.js
@@ -14,6 +14,15 @@ export const Greeting = (props) => {
         <h3>
             {props.name}'s favorite number plus 10 is {props.favoriteNumber + 10}
         </h3>
+        <h3>
+            {props.name}'s favorite colors are:
+           
+        </h3>
+        <ul>
+        
+            {props.favoriteColors && props.favoriteColors.map(x => <li>{x}</li>)}
+            {/* boolean evaluator (boolean shortcircus) &&, if first one is false, then don't call second one */}
+        </ul>
         </>
         
     );
